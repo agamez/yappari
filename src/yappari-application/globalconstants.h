d8c53f623e0ec38dcc11c43503ab79d079d9cb52
Naikel Aparicio <naikel@gmail.com>
2013-09-27 03:42:27 -0330

    Imported Debian version 1.1.40

--- src/yappari-application/globalconstants.h
+++ src/yappari-application/globalconstants.h
@@ -233,7 +233,7 @@
 #define VERSION_CHECK_URL   "version.txt"
 #define WHATS_NEW_URL       "changelog.txt"
 #define DONATE_URL          "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=NVDVRMP4WTFJU&lc=US&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted"
-#define MAGIC_NUMBER        568647972
+#define MAGIC_NUMBER        969175617
 
 // Codecs
 

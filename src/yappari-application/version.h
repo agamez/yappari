141ab59db7870e3c32c4502faf397fe2128cd18d
Naikel Aparicio <naikel@gmail.com>
2014-08-23 13:17:03 -0330

    Imported Debian version 1.1.48
      * Fixed most of the Windows Phone/iPhone emojis.

--- src/yappari-application/version.h
+++ src/yappari-application/version.h
@@ -1,3 +1,3 @@
-#define BUILD_NUMBER "4659"
-#define VERSION "1.1.47"
-#define FULL_VERSION "1.1.47 Build 4659"
+#define BUILD_NUMBER "4771"
+#define VERSION "1.1.48"
+#define FULL_VERSION "1.1.48 Build 4771"

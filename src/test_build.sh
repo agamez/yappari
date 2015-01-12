f0dc60f64f7fdae5755d3361288d7db4bc3031c9
Alvaro G. M <alvaro.gamez@hazent.com>
2015-01-12 16:37:51 +0100

    test_build.sh script modified to follow new conventions such as  instead of  and clone.sh file integrated inside this script and other minor modifications.

--- src/test_build.sh
+++ src/test_build.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-expr `cat yappari-build` + 1 >yappari-build
-echo \#define BUILD_NUMBER \"`cat yappari-build`\" >src/yappari-application/version.h
-echo \#define VERSION \"`head -1 src/qtc_packaging/debian_fremantle/changelog.original | sed "s/.*(\(.*\)).*/\1/"`\" >>src/yappari-application/version.h
-echo \#define FULL_VERSION \"`head -1 src/qtc_packaging/debian_fremantle/changelog.original | sed "s/.*(\(.*\)).*/\1/"` Build `cat yappari-build`\" >>src/yappari-application/version.h
-clone src build-scratchbox
-echo DEFINES += Q_WS_SCRATCHBOX=1 >>build-scratchbox/yappari-application/yappari-application.pro
+expr $(cat yappari-build) + 1 >yappari-build
+echo \#define BUILD_NUMBER \"$(cat yappari-build)\" > yappari-application/version.h
+echo \#define VERSION \"$(head -1 ../debian/changelog | sed "s/.*(\(.*\)).*/\1/")\" >> yappari-application/version.h
+echo \#define FULL_VERSION \"$(head -1 ../debian/changelog | sed "s/.*(\(.*\)).*/\1/") Build \" BUILD_NUMBER >> yappari-application/version.h
+
+mkdir build-scratchbox
+tar cpf - . --exclude=build-scratchbox | tar xpf - -C build-scratchbox
+
+echo DEFINES += Q_WS_SCRATCHBOX=1 >> build-scratchbox/yappari-application/yappari-application.pro
 cd build-scratchbox
 qmake yappari.pro
 make
 cd ..
-cp src/yappari-application/share/whatsnew.txt /usr/share/yappari
+cp yappari-application/share/whatsnew.txt /usr/share/yappari
 if [ -z $1 ]; then
     run-standalone.sh build-scratchbox/yappari-application/Yappari
 fi

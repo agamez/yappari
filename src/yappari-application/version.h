ac00fe3af16ed422f5934d671870fb9ccbe6312d
Alvaro G. M <alvaro.gamez@hazent.com>
2015-01-17 17:24:54 +0100

    Bump versions for testing release

--- src/yappari-application/version.h
+++ src/yappari-application/version.h
@@ -1,6 +1,6 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-#define BUILD_NUMBER 4834
+#define BUILD_NUMBER 4837
 #define VERSION "1.1.53"
 #define FULL_VERSION VERSION " Build " STR(BUILD_NUMBER)

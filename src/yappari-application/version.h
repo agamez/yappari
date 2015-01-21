ee1bcce114e99782833bb3d0aba1134e750864bd
Alvaro G. M <alvaro.gamez@hazent.com>
2015-01-21 11:50:09 +0100

    Update changelog, whatsnew.txt, version.h

--- src/yappari-application/version.h
+++ src/yappari-application/version.h
@@ -1,6 +1,6 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-#define BUILD_NUMBER 4837
+#define BUILD_NUMBER 4840
 #define VERSION "1.1.53"
 #define FULL_VERSION VERSION " Build " STR(BUILD_NUMBER)

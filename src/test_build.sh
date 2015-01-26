2791ca4083d162554f063a982ad3f5caffcd9070
Alvaro G. M <alvaro.gamez@hazent.com>
2015-01-26 11:03:07 +0100

    test_build.sh: finish execution on compiling error

--- src/test_build.sh
+++ src/test_build.sh
@@ -19,9 +19,16 @@ if [ -z $1 ]; then
 	echo DEFINES += Q_WS_SCRATCHBOX=1 >> build-scratchbox/yappari-application/yappari-application.pro
 fi
 cd build-scratchbox
-qmake yappari.pro
-make
+
+qmake yappari.pro && make
+RET=$?
+
 cd ..
+
+if [ $RET -ne 0 ]; then
+	exit 1
+fi
+
 cp yappari-application/share/whatsnew.txt /usr/share/yappari
 if [ -z $1 ]; then
     run-standalone.sh build-scratchbox/yappari-application/Yappari

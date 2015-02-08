e66b0abd67e5af54386d73e5fbbf72f250165cb1
Alvaro G. M <alvaro.gamez@hazent.com>
2015-02-08 16:30:29 +0100

    Prepared for release 2.0.5: upgraded changelog, whatsnew.txt and version.h

--- src/yappari-application/version.h
+++ src/yappari-application/version.h
@@ -1,6 +1,6 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-#define BUILD_NUMBER 5204
-#define VERSION "2.0.4"
+#define BUILD_NUMBER 5205
+#define VERSION "2.0.5"
 #define FULL_VERSION VERSION " Build " STR(BUILD_NUMBER)

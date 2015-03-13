9f0214541f900c5edd6ef14bd2c605f4c4d19ca9
Alvaro G. M <alvaro.gamez@hazent.com>
2015-03-13 16:59:51 +0100

    Prepared for release 2.0.9: upgraded changelog, whatsnew.txt and version.h
    Signed-off-by: Alvaro G. M <alvaro.gamez@hazent.com>

--- src/yappari-application/version.h
+++ src/yappari-application/version.h
@@ -1,6 +1,6 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-#define BUILD_NUMBER 5208
-#define VERSION "2.0.8"
+#define BUILD_NUMBER 5209
+#define VERSION "2.0.9"
 #define FULL_VERSION VERSION " Build " STR(BUILD_NUMBER)

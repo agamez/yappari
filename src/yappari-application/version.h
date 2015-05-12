770bc27c07a268f8658891f10a3d9ee9e212cda2
Alvaro G. M <alvaro.gamez@hazent.com>
2015-05-12 11:10:59 +0200

    Prepare for release 2.0.12-1

--- src/yappari-application/version.h
+++ src/yappari-application/version.h
@@ -1,6 +1,6 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-#define BUILD_NUMBER 5212
+#define BUILD_NUMBER 5213
 #define VERSION "2.0.12"
 #define FULL_VERSION VERSION " Build " STR(BUILD_NUMBER)

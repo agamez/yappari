b0458ac6ad9db05dca95ab057ff8e414a96a4b7a
Alvaro G. M <alvaro.gamez@hazent.com>
2015-07-06 14:15:32 +0200

    Prepared for release 2.0.15: upgraded WA version number; changelog, whatsnew.txt and version.h

--- src/yappari-application/version.h
+++ src/yappari-application/version.h
@@ -1,6 +1,6 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-#define BUILD_NUMBER 5215
-#define VERSION "2.0.14"
+#define BUILD_NUMBER 5216
+#define VERSION "2.0.15"
 #define FULL_VERSION VERSION " Build " STR(BUILD_NUMBER)
